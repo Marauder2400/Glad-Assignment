6904494dd7d7398465af95b2ed01fea7f3f97a53
Sonia Sangtani <soniasangtani402@gmail.com>
2015-08-23 17:20:07 +0400

    Create trialFilter.php

---
+++ trialFilter.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Filters by me </title>
+
+	<style type="text/css">
+		.ig-willow {
+  -webkit-filter: saturate(0.02) contrast(0.85) brightness(1.2) sepia(0.02);
+  filter: saturate(0.02) contrast(0.85) brightness(1.2) sepia(0.02);
+}
+
+	</style>
+
+	  <script type="text/javascript">
+        function ani(){
+            document.getElementById('img').className ='ig-willow';
+        }
+  </script>
+</head>
+<body>
+
+<img id="img" src="./img/Cake.jpg" >
+
+<input name="" type="button" onclick="ani()" />
+
+</body>
+</html>
